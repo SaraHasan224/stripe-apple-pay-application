349d8f9ab293333784368d5ea1c1c95f0e0df9f5
sara.hasan <sara.hasan@nextgeni.com>
2024-05-24 08:19:32 +0100

    Added port in env file

--- public/checkout.js
+++ public/checkout.js
@@ -45,7 +45,7 @@ async function handleSubmit(e) {
     elements,
     confirmParams: {
       // Make sure to change this to your payment completion page
-      return_url: "http://localhost:4242/checkout.html",
+      return_url: process.env.APP_URL + "/checkout.html",
     },
   });
 
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
-const YOUR_DOMAIN = "http://localhost:4242";
-
 app.post("/create-checkout-session", async (req, res) => {
   let price;
   (async () => {
@@ -29,9 +27,9 @@ app.post("/create-checkout-session", async (req, res) => {
           quantity: 5,
         },
       ],
-      mode: "payment",
-      success_url: `${YOUR_DOMAIN}/success.html`,
-      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+      mode: "subscription",
+      success_url: process.env.APP_URL + `/success.html`,
+      cancel_url: process.env.APP_URL + `/cancel.html`,
       automatic_tax: { enabled: true },
     });
     console.log("session: ", session);
@@ -39,4 +37,167 @@ app.post("/create-checkout-session", async (req, res) => {
   })();
 });
 
-app.listen(4242, () => console.log("Running on port 4242"));
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_078b9815d705873a4072adbd0603190a6281a3746048b6a6e8c74ee09242c19a";
+
+app.post(
+  "/new-webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "customer.created":
+        const customerCreated = event.data.object;
+        // Then define and call a function to handle the event customer.created
+        break;
+      case "customer.deleted":
+        const customerDeleted = event.data.object;
+        // Then define and call a function to handle the event customer.deleted
+        break;
+      case "customer.updated":
+        const customerUpdated = event.data.object;
+        // Then define and call a function to handle the event customer.updated
+        break;
+      case "customer.discount.created":
+        const customerDiscountCreated = event.data.object;
+        // Then define and call a function to handle the event customer.discount.created
+        break;
+      case "customer.discount.deleted":
+        const customerDiscountDeleted = event.data.object;
+        // Then define and call a function to handle the event customer.discount.deleted
+        break;
+      case "customer.discount.updated":
+        const customerDiscountUpdated = event.data.object;
+        // Then define and call a function to handle the event customer.discount.updated
+        break;
+      case "customer.source.created":
+        const customerSourceCreated = event.data.object;
+        // Then define and call a function to handle the event customer.source.created
+        break;
+      case "customer.source.deleted":
+        const customerSourceDeleted = event.data.object;
+        // Then define and call a function to handle the event customer.source.deleted
+        break;
+      case "customer.source.expiring":
+        const customerSourceExpiring = event.data.object;
+        // Then define and call a function to handle the event customer.source.expiring
+        break;
+      case "customer.source.updated":
+        const customerSourceUpdated = event.data.object;
+        // Then define and call a function to handle the event customer.source.updated
+        break;
+      case "customer.subscription.created":
+        const customerSubscriptionCreated = event.data.object;
+        // Then define and call a function to handle the event customer.subscription.created
+        break;
+      case "customer.subscription.deleted":
+        const customerSubscriptionDeleted = event.data.object;
+        // Then define and call a function to handle the event customer.subscription.deleted
+        break;
+      case "customer.subscription.paused":
+        const customerSubscriptionPaused = event.data.object;
+        // Then define and call a function to handle the event customer.subscription.paused
+        break;
+      case "customer.subscription.pending_update_applied":
+        const customerSubscriptionPendingUpdateApplied = event.data.object;
+        // Then define and call a function to handle the event customer.subscription.pending_update_applied
+        break;
+      case "customer.subscription.pending_update_expired":
+        const customerSubscriptionPendingUpdateExpired = event.data.object;
+        // Then define and call a function to handle the event customer.subscription.pending_update_expired
+        break;
+      case "customer.subscription.resumed":
+        const customerSubscriptionResumed = event.data.object;
+        // Then define and call a function to handle the event customer.subscription.resumed
+        break;
+      case "customer.subscription.trial_will_end":
+        const customerSubscriptionTrialWillEnd = event.data.object;
+        // Then define and call a function to handle the event customer.subscription.trial_will_end
+        break;
+      case "customer.subscription.updated":
+        const customerSubscriptionUpdated = event.data.object;
+        // Then define and call a function to handle the event customer.subscription.updated
+        break;
+      case "customer.tax_id.created":
+        const customerTaxIdCreated = event.data.object;
+        // Then define and call a function to handle the event customer.tax_id.created
+        break;
+      case "customer.tax_id.deleted":
+        const customerTaxIdDeleted = event.data.object;
+        // Then define and call a function to handle the event customer.tax_id.deleted
+        break;
+      case "customer.tax_id.updated":
+        const customerTaxIdUpdated = event.data.object;
+        // Then define and call a function to handle the event customer.tax_id.updated
+        break;
+      case "issuing_card.created":
+        const issuingCardCreated = event.data.object;
+        // Then define and call a function to handle the event issuing_card.created
+        break;
+      case "issuing_card.updated":
+        const issuingCardUpdated = event.data.object;
+        // Then define and call a function to handle the event issuing_card.updated
+        break;
+      case "issuing_transaction.created":
+        const issuingTransactionCreated = event.data.object;
+        // Then define and call a function to handle the event issuing_transaction.created
+        break;
+      case "issuing_transaction.updated":
+        const issuingTransactionUpdated = event.data.object;
+        // Then define and call a function to handle the event issuing_transaction.updated
+        break;
+      case "payment_intent.amount_capturable_updated":
+        const paymentIntentAmountCapturableUpdated = event.data.object;
+        // Then define and call a function to handle the event payment_intent.amount_capturable_updated
+        break;
+      case "payment_intent.canceled":
+        const paymentIntentCanceled = event.data.object;
+        // Then define and call a function to handle the event payment_intent.canceled
+        break;
+      case "payment_intent.created":
+        const paymentIntentCreated = event.data.object;
+        // Then define and call a function to handle the event payment_intent.created
+        break;
+      case "payment_intent.partially_funded":
+        const paymentIntentPartiallyFunded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.partially_funded
+        break;
+      case "payment_intent.payment_failed":
+        const paymentIntentPaymentFailed = event.data.object;
+        // Then define and call a function to handle the event payment_intent.payment_failed
+        break;
+      case "payment_intent.processing":
+        const paymentIntentProcessing = event.data.object;
+        // Then define and call a function to handle the event payment_intent.processing
+        break;
+      case "payment_intent.requires_action":
+        const paymentIntentRequiresAction = event.data.object;
+        // Then define and call a function to handle the event payment_intent.requires_action
+        break;
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
+
+app.listen(process.env.PORT, () => console.log("Running on port 4242"));
